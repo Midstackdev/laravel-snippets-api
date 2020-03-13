a41cdf7e028d3bb669b2d7e28cd0c5b2cfe56b76
Midstackdev <midstackdev@gmail.com>
2020-03-13 19:10:56 +0000

    SignIn user

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
     Route::post('login', 'SignInController');
+    Route::post('logout', 'SignOutController');
     Route::get('me', 'MeController');
 });
